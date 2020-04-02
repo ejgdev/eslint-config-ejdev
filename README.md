5b9a6fe7b1250f7773ef7c54d5d62109f990d8d5
Edward Jimenez <ejg.dev@gmail.com>
2020-04-02 16:16:34 -0500

    First Commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# eslint-config-ejgdev
+
+Guide lines for eslint
---
+++ node.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+       'airbnb-base',
+   ],
+ globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    semi: ['error', 'always'] // https://eslint.org/docs/rules/semi
+  }
+};
