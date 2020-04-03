587396b6f97a2edddea5aad592451bce7ea97c43
Edward Jimenez <ejg.dev@gmail.com>
2020-04-03 17:47:04 -0500

    fixing files issues | adding own eslintrc fie :)

--- native.js
+++ native.js
@@ -1,21 +1,21 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true,
-    "es2017": true,
-    "jest": true,
-    "node": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    es2017: true,
+    jest: true,
+    node: true,
   },
-  "parser": "babel-eslint",
-  "extends": ["airbnb"],
-  "plugins": ["react", "import"],
-  "rules": {
-    "react/jsx-filename-extension": ["off"],
-    "linebreak-style": ["off"],
-    "no-undef": ["error"],
-    "react/sort-comp": ["off"],
-    "react/prefer-stateless-function": ["off"],
-    "jsx-a11y/anchor-is-valid": ["off"]
+  parser: 'babel-eslint',
+  extends: ['airbnb'],
+  plugins: ['react', 'import'],
+  rules: {
+    'react/jsx-filename-extension': ['off'],
+    'linebreak-style': ['off'],
+    'no-undef': ['error'],
+    'react/sort-comp': ['off'],
+    'react/prefer-stateless-function': ['off'],
+    'jsx-a11y/anchor-is-valid': ['off'],
   },
 };
--- node.js
+++ node.js
@@ -1,19 +1,21 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true,
-    "es2017": true,
-    "jest": true,
-    "node": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    es2017: true,
+    jest: true,
+    node: true,
   },
-  "extends": ["airbnb-base"],
-  "parser-options": {
-    "ecmaVersion": 2018
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 2018,
   },
-  "rules": {
-    "curly": "error",
-    "brace-style": ["error", "1tbs", { "allowSingleLine": false }],
-    "object-curly-newline": ["error", "always"]
-  }
+  rules: {
+    curly: 'error',
+    'brace-style': ['error', '1tbs', {
+      allowSingleLine: false,
+    }],
+    'object-curly-newline': ['error', 'always'],
+  },
 };
